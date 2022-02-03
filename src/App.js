8a576e2b191d8e25b1001e9b9bfc17fcdca3c4a6
Michael Zhu <mzhu23@gmail.com>
2022-02-03 04:33:44 -0800

    Added semantic UI button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import theme from "styled-theming";
 import ButtonVariants from "./components/ButtonVariants";
 import { StyledThemingButton } from "./components/StyledThemingButton";
 import { ThemeObjectButton } from "./components/ThemeObjectButton";
+import { Button } from "semantic-ui-react";
 
 function App() {
   return (
@@ -23,9 +26,26 @@ function App() {
         <StyledThemingButton />
         <ButtonVariants />
         <ThemeObjectButton />
+        <StyledSemanticButton>Click Here</StyledSemanticButton>
       </header>
     </div>
   );
 }
+
+const backgroundColor = theme("mode", {
+  light: "yellow",
+  dark: "hotPink",
+});
+
+const width = theme("mode", {
+  light: "300px",
+  dark: "500px",
+});
+
+const StyledSemanticButton = styled(Button)`
+  background-color: ${backgroundColor};
+  width: ${width};
+  height: 50px;
+`;
 
 export default App;
